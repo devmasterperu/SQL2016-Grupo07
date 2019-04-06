d3836c0578f1cd0c18265a7ca3ec2615451e013a
Gianfranco Manrique <info@devmaster.pe>
2019-04-06 09:50:01 -0500

    Update parte-01.sql

--- Semana-06/parte-01.sql
+++ Semana-06/parte-01.sql
@@ -22,4 +22,46 @@ from tb_Colaborador c
 inner join tb_TipoDocumento t on c.idTipoDocumento=t.idTipoDocumento
 ) persona
 
---1.2 Mostrar las personas indicando a que tabla pertenecen
+--1.2 Mostrar los nombres y apellidos de personas indicando a que tabla pertenecen y además, 
+--mostrar un registro con los datos 'NN' como nombres, 'NN' como apellidos y que no pertenece a 
+--ninguna tabla.
+select nombreCliente as nombres,apellidosCliente as apellidos,'tb_Cliente' as tabla
+from tb_Cliente
+union
+select nombreColaborador as nombres,apellidoColaborador as apellidos,'tb_Colaborador' as tabla
+from tb_Colaborador
+union
+select 'NN' as nombres,'NN' as apellidos,'Ninguno' as tabla
+
+--1.3 Con CTE
+WITH CTE_personas
+AS
+(
+select nombreCliente as nombres,apellidosCliente as apellidos,'tb_Cliente' as tabla
+from tb_Cliente
+union
+select nombreColaborador as nombres,apellidoColaborador as apellidos,'tb_Colaborador' as tabla
+from tb_Colaborador
+union
+select 'NN' as nombres,'NN' as apellidos,'Ninguno' as tabla
+)
+Select nombres, apellidos, tabla from CTE_personas
+
+--1.4 Con función de valor tabla
+create function dbo.fnPersonas() returns table
+as
+return
+WITH CTE_personas
+AS
+(
+select nombreCliente as nombres,apellidosCliente as apellidos,'tb_Cliente' as tabla
+from tb_Cliente
+union
+select nombreColaborador as nombres,apellidoColaborador as apellidos,'tb_Colaborador' as tabla
+from tb_Colaborador
+union
+select 'NN' as nombres,'NN' as apellidos,'Ninguno' as tabla
+)
+Select nombres, apellidos, tabla from CTE_personas
+
+select * from dbo.fnPersonas()
