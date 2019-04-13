71a7a4e232f1b6d7e843e690639d73fbf451982e
gmanriquev <info@devmaster.pe>
2019-04-13 13:35:40 -0500

    Parte 03

--- Semana-07/parte-03.sql
+++ Semana-07/parte-03.sql
@@ -54,3 +54,30 @@ contacto.idProducto
 from tb_ContactoCliente  contacto
 inner join tb_Cliente  cliente on cliente.idCliente=contacto.idCliente
 for xml auto,root('clientes')
+
+--7
+--alter view vMetas
+--as
+select r.nombreRol,s.nombreSemanaPlan,p.nombreProducto,m.totalMeta 
+from tb_Meta m
+inner join tb_Rol r on m.idRol=r.idRol
+inner join tb_SemanaPlanificada s on m.idSemanaPlan=s.idSemanaPlan
+inner join tb_Producto p on m.idProducto=p.idProducto
+for xml auto, root('metas')
+
+--XML PATH
+select 
+r.nombreRol as '@rol',--regla 1
+m.totalMeta as 'semana/@totalMeta',--regla 1+regla 3
+s.nombreSemanaPlan as 'semana/@nombre',
+p.nombreProducto as 'semana/producto',--regla 3,
+p.estadoProducto --regla 2
+from tb_Meta m
+inner join tb_Rol r on m.idRol=r.idRol
+inner join tb_SemanaPlanificada s on m.idSemanaPlan=s.idSemanaPlan
+inner join tb_Producto p on m.idProducto=p.idProducto
+for XML path('meta')
+
+
+
+
